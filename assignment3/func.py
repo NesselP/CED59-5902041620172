42008fa7fc32a0a64b826c06d46789a301052e41
PHUMIN CHUOYRUM <s5902041620172@Macmini-213-05.local>
2019-07-11 14:39:33 +0700

    add files for assignment3

---
+++ assignment3/func.py
@@ -0,0 +1,5 @@
+def return_():
+   return 42;
+def mymethod(a,b):
+   retuen a*b;
+
---
+++ assignment3/main.py
@@ -0,0 +1,5 @@
+from func import*
+
+print("Result of return_42() is ", return_42());
+
+print("Result of my_method(a,b) is ",my_method(5,6));
