62eb13fb99fcf3f9b1c3431a9d556209f24e929b
NesselP <52252265+NesselP@users.noreply.github.com>
2019-08-08 00:11:37 +0700

    Add files via upload

---
+++ Assignment6/main.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template('register.html')
+
+
+@app.route('/', methods=["post"])
+def register():
+    return 'ลงทะเบียนสำเร็จ'
+
+
+if __name__ == '__main__':
+    app.run()
