d9eecce7010c71deba67236a6d097416090089e3
NesselP <52252265+NesselP@users.noreply.github.com>
2019-08-22 01:38:15 +0700

    Update main.py

--- assignment7/main.py
+++ assignment7/main.py
@@ -1,19 +1,32 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask,render_template , request
+from wtforms import StringField,PasswordField
+from flask_wtf import FlaskForm
+from wtforms.validators import InputRequired, Length, Email
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'any secret string'
+
+class RegisterForm(FlaskForm):
+    name = StringField("name", validators=[InputRequired()])
+    last = StringField("last", validators=[InputRequired()])
+    email = StringField("Email",  [InputRequired("Please enter your email address."), Email("Please enter your email again.")])
+    username = StringField("username", validators=[InputRequired()])
+    password = PasswordField("password", validators=[InputRequired(), Length(min=8,max=12, message="Please enter your password 8-12 characters.")])
+
+@app.route('/')
+def member():
+    form = RegisterForm()
+    return render_template('register.html', form=form)
 
 
-@app.route("/")
-def index():
-    return render_template('register.html')
-
-
-@app.route('/', methods=["post"])
+@app.route('/register', methods=["GET", "POST"])
 def register():
-    return 'ลงทะเบียนสำเร็จ'
+    form = RegisterForm()
+    if form.validate_on_submit():
+        return "ลงทะเบียนสำเร็จ"
+    return render_template('register.html', form=form)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
